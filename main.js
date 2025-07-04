63bf2da70ed52c3955545d137a53c4d06a16c2e5
RufHack <raflirust49@gmail.com>
2025-07-04 10:51:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".btnDetail").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let parent = e.target.parentNode.parentNode;
+
+    let gambar = parent.querySelector(".card-img-top").src;
+    let harga = parent.querySelector(".harga").innerHTML;
+    let judul = parent.querySelector('.card-text').textContent;
+    let deskripsi = parent.querySelector(".deskripsi")
+      ? parent.querySelector(".deskripsi").innerHTML
+      : "<i>tidak ada informasi yang tersedia</i>";
+
+    let tombolModal = document.querySelector(".btnModal");
+    tombolModal.click();
+
+    document.querySelector(".modalTitle").innerHTML = judul;
+    let image = document.createElement("img");
+    image.src = gambar;
+    image.classList.add("w-100");
+    document.querySelector(".modalImage").innerHTML = "";
+    document.querySelector(".modalImage").appendChild(image);
+    document.querySelector(".modalDeskripsi").innerHTML = deskripsi;
+    document.querySelector(".modalHarga").innerHTML = harga;
+
+    const nohp = "+62 895-3976-33383";
+    let teksPesan = `Halo Bang, saya ingin membeli produk *${judul.trim()}* dengan harga *${harga.trim()}*. Apakah masih tersedia?`;
+    let linkWA = `https://api.whatsapp.com/send?phone=${nohp}&text=${encodeURIComponent(
+      teksPesan
+    )}`;
+    document.querySelector(".btnBeli").href = linkWA;
+
+    document.querySelector(".btnBeli").href = pesan;
+    
+  });
+});
